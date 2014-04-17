2b02cf96ff4616816d18c0280a1824b4d8a02a06
wataru.abe <wataru.abe@mixi.co.jp>
2014-04-17 17:21:15 +0900

    version up

--- lib/error_handle_filter.rb
+++ lib/error_handle_filter.rb
@@ -1,17 +1,15 @@
 require "error_handle_filter/version"
 
-module ErrorHandleFilter
-  class ErrorHandleFilter
-    def initialize app
-      @app = app
-    end
+class ErrorHandleFilter
+  def initialize app
+    @app = app
+  end
 
-    def call env
-      begin
-        @app.call(env)
-      rescue
-        [500, { 'Content-Type' => 'application/json' }, [JSON.dump(message: "unexpected error")]]
-      end
+  def call env
+    begin
+      @app.call(env)
+    rescue
+      [500, { 'Content-Type' => 'application/json' }, [JSON.dump(message: "unexpected error")]]
     end
   end
 end
